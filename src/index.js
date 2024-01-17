558d3e1fb457cd95a38f4bea8e615a2e149676c8
Taisaka37 <125468545+Taisaka37@users.noreply.github.com>
2024-01-17 13:28:02 +0100

    did stuff

--- src/index.js
+++ src/index.js
@@ -1,58 +1,85 @@
-// const Tasks = 
-
 const projectsUpperContainer = document.querySelector('[data-projects]')
+const tasksContainer = document.querySelector('[data-board]')
 const newProjectForm = document.querySelector('[data-new-project-form]')
+const newTaskForm = document.querySelector('[data-new-task-form]')
 const newProjectInput = document.querySelector('[data-new-project-input]')
+const newTaskInput = document.querySelector('[data-new-Task-input]')
+const dueDateInput = document.querySelector('[data-dueDate-input]')
+const priorityInput = document.querySelector('[data-priority-input]')
 
 const LOCAL_STORAGE_PROJECT_KEY = 'task.projects'
+const LOCAL_STORAGE_TASK_KEY = 'task.tasks'
+// need to store tasks??????
+//
+//
 const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = 'task.selectedProjectId'
 
+// My projects are stored in projects and they're taken from localStorage if available
 let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [{
   id: 1,
   name: 'Inbox',
-  tasks: []
 }, {
   id: 2,
   name: 'Today',
-  tasks: []
 }, {
   id: 3,
   name: 'Upcoming',
-  tasks: []
 }];
 
+let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
+
+// I get selectedProjectID
 let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY)
-console.log(selectedProjectId)
 
+// I add an event listener that changes selectedProjectID on click
 projectsUpperContainer.addEventListener('click', e => {
-  if (e.target.className === 'projects-container' ) {
-    selectedProjectId = e.target.querySelector('div').dataset.projectId          
-    console.log(selectedProjectId)
-    console.log(projects)
+  if (e.target.className === 'projects-element' ) {
+    selectedProjectId = e.target.dataset.projectId 
     saveAndRender()
-
   }
 })
-// const projectsContainer = document.querySelectorAll('.projects-container');
-// projectsContainer.forEach(project => {
-//   project.addEventListener('click', () => {
-//     document.querySelector('.active-project')?.classList.remove('active-project');
-//     project.classList.add('active-project')
-//   })
-// })
 
+// When I submit a project it's added to projects
 newProjectForm.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
   const projectName = newProjectInput.value;
   if (projectName == null || projectName === '') return;
-  const project = createProject(projectName)
-  newProjectInput.value = null
-  projects.push(project)
-  saveAndRender()
+  const project = createProject(projectName);
+  newProjectInput.value = null;
+  projects.push(project);
+  saveAndRender();
+});
+
+newTaskForm.addEventListener('input', e=> {
+    const taskOptions = document.querySelector('.side');
+    taskOptions.style.display = "flex";
+    const taskName = newTaskInput.value;
+    if (taskName == null || taskName === '') {
+        taskOptions.style.display = "none"
+    }
+    dueDateInput.min = new Date().toISOString().split("T")[0];
+});
+
+newTaskForm.addEventListener('submit', e=> {
+    e.preventDefault()
+    const taskOptions = document.querySelector('.side');
+    taskOptions.style.display = "none";
+    const taskName = newTaskInput.value;
+    if (taskName == null || taskName === '') return;
+    let temp = Date.parse(dueDateInput.value)
+    const taskDueDate = new Date(temp).toLocaleDateString();
+    const taskPriority = priorityInput.value;
+    const task = createTask(taskName, taskDueDate, taskPriority);
+    newTaskInput.value = null;
+    dueDateInput.value = null;
+    // priorityInput.value = null;
+    tasks.push(task);
+    saveAndRender();
 })
 
+// Can I add tasks to a project somehow? project.tasks
 function createProject(name) { 
-  return { id: Date.now().toString(), name, tasks: []}
+  return { id: Date.now().toString(), name}
 }
 
 function saveAndRender() {
@@ -63,60 +90,93 @@ function saveAndRender() {
 function save() {
   localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects))
   localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY, selectedProjectId)
+  localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasks))
 }
 function render() {
   clearElement(projectsUpperContainer)
     projects.forEach(project => {
-    const projectsLowerContainer = document.createElement('div');
     const projectsElement = document.createElement('div');
-    projectsLowerContainer.classList.add('projects-container');
     projectsElement.classList.add('projects-element')
     if (project.id == selectedProjectId) {
       projectsElement.classList.add('active-project')
     }
-    projectsUpperContainer.append(projectsLowerContainer);
-    projectsLowerContainer.append(projectsElement);
+    projectsUpperContainer.append(projectsElement);
     projectsElement.innerText = project.name;
     projectsElement.dataset.projectId = project.id;
-    // console.log(projectsElement)
-    // projectsElement.addEventListener('click', () => {
-    //   document.querySelector('.active-project')?.classList.remove('active-project');
-    //   projectsElement.classList.add('active-project')
-    // })
-
   })
-}
+  clearElement(tasksContainer)
+  tasks.forEach(task => {
+    if (task.id != selectedProjectId) return;
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('checkbox-container');
+    const taskDiv2 = document.createElement('div');
+    taskDiv.append(taskDiv2);
 
-function addSwitchActive() {
+    switch (task.priority) {
+        case 'high':
+            taskDiv.classList.add('high-priority');
+            break;
+        case 'medium':
+            taskDiv.classList.add('mid-priority');
+            break;
+        case 'low':
+            taskDiv.classList.add('low-priority');
+            break;
+    }
+    console.log(task);
+    console.log(tasks.indexOf(task));
+    tasksContainer.append(taskDiv);
+    const taskInput = document.createElement('input');
+    const taskLabel = document.createElement('label');
+    taskInput.id = task.label;
+    taskInput.type = 'checkbox';
+    taskLabel.setAttribute('for', task.label)
+    
+    taskLabel.innerText = `${task.name} \n Due: ${task.dueDate}`
+    if (task.dueDate == '' || task.dueDate == 'Invalid Date') {
+        taskLabel.innerText = task.name
 
+    }
+    console.log(task.dueDate);
+    taskDiv2.append(taskInput);
+    taskDiv2.append(taskLabel);
+    const btn = document.createElement('button');
+    taskDiv.append(btn);
+    btn.innerText = 'x';
+    btn.classList.add('invis')
+    btn.id = task.label;
+
+    btn.addEventListener('click', e => {
+        tasks.splice(tasks.indexOf(task), 1);
+        saveAndRender();
+    })
+
+    taskLabel.addEventListener('click', e=> {
+        
+        sleep(500).then(() => { completeTask(task); });;
+    })
+    
+})
 }
 
 function clearElement(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild)
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+
+function createTask (name, dueDate, priority) {
+    
+    return { id: selectedProjectId, name, dueDate, priority, label: Date.now().toString()}
+}
+
+function completeTask(task) {
+    tasks.splice(tasks.indexOf(task), 1);
+    saveAndRender();
+    
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
-}
-
-function createTask (title, description, dueDate, priority) {
-
-  return { id: Date.now().toString(), title, description, dueDate, priority, complete: false }
-}
-
-function completeTask() {
-
-}
-
-function addProject() {
-
-}
-
-function addSection() {
-
-}
-
-
-const Task1 = createTask('work', 'gotta work', Date.now(), 'ultra');
-
-console.log(Task1);
-
 render();
